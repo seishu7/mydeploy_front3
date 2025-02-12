6be2ae9e4707e72b6b0e015ef7ea3303077572f4
seishu <oltoberry7@gmail.com>
2025-02-13 00:08:13 +0900

    0212ymlデプロイ

--- next.config.js
+++ next.config.js
@@ -1,10 +1,23 @@
+//require('dotenv').config()
+//** @type {import('next').NextConfig} */
+//const nextConfig = {
+//    env: {
+        // Reference a variable that was defined in the .env file and make it available at Build Time
+//        API_ENDPOINT: process.env.NEXT_PUBLIC_API_ENDPOINT,
+//      },
+//}
+
+//module.exports = nextConfig
+
 require('dotenv').config()
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env: {
-        // Reference a variable that was defined in the .env file and make it available at Build Time
+         //Reference a variable that was defined in the .env file and make it available at Build Time
         API_ENDPOINT: process.env.NEXT_PUBLIC_API_ENDPOINT,
-      },
-}
+    }
+  }
 
-module.exports = nextConfig
+module.exports = {
+  output: "standalone",
+};
